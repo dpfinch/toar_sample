89abe906fd601b1810865ec314d936cd67a6aaf4
dpfinch <d.finch@ed.ac.uk>
2021-06-30 12:46:24 +0100

    Added structural folders

---
+++ config.py
@@ -0,0 +1,5 @@
+################################################################################
+#################### Configuration file for TOAR sampling code #################
+
+output_dir = ''
+output_filename_prefix = ''
